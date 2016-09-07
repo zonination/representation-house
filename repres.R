f051452ba09c1f7976b76ea583fb039a91e7429d
Zoni Nation <zonination@gmail.com>
2016-09-06 23:11:00 -0400

    Add files via upload

--- repres.R
+++ repres.R
@@ -15,14 +15,16 @@ for(n in 1790:2010){
 }
 df<-df[2:nrow(df),]
 
-df$ratio<-df$nrep*100000/df$uspop
+df$ratio<-df$nrep*30000/df$uspop
 
 ggplot(df,aes(year,ratio))+
   geom_line(color="firebrick3",size=2)+
-  geom_smooth(method=lm,color="black",size=.5,linetype=4,se=F)+
-  scale_y_log10(breaks=c(.1,.2,.5,1,2,5,10),limits=c(.1,3),minor_breaks=c(seq(.1,1,.1),seq(1,10,1)))+
-  scale_x_continuous(breaks=seq(1800,2000,50),minor_breaks=seq(1790,2010,10))+
-  ylab("Number of House Members per 100,000 People")+
+  # geom_smooth(method=lm,color="black",size=.5,linetype=4,se=F)+
+  # scale_y_log10(breaks=c(01,.02,.05,.1,.2,.5,1,2,5,10),
+  #               labels=as.character(c(01,.02,.05,.1,.2,.5,1,2,5,10)),
+  #               limits=c(.02,1),minor_breaks=c(seq(.01,.1,.01),seq(.1,1,.1),seq(1,10,1)))+
+  # scale_x_continuous(breaks=seq(1800,2000,50),minor_breaks=seq(1790,2010,10))+
+  ylab("Number of House Members per 30,000 People")+
   xlab("Year")+
   ggtitle("Representation in the U.S.")+
   theme_bw()
